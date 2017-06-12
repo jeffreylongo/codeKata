2f448b9cff9cb82ff97338f0954b43bd98a0451f
Jeff Longo <jeffreylongo@yahoo.com>
2017-06-12 15:28:10 -0400

    Hacker Rank: CamelCase

---
+++ HRCamelCase/HRCamelCase/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        Console.WriteLine((Console.ReadLine().Count(i => char.IsUpper(i)) + 1));
+
+    }
+}
