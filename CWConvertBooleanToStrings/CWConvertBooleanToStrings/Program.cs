58acd72b8df9fcc8bd72864cdf185268c77de7d8
Jeff Longo <jeffreylongo@yahoo.com>
2017-06-12 15:13:28 -0400

    Code Wars: Convert Booleans to Strings

---
+++ CWConvertBooleanToStrings/CWConvertBooleanToStrings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    bool word = false;
+    public static string boolToWord(bool word)
+    {
+        if (word == true)
+        {
+            return "Yes";
+        }
+        else
+        {
+            return "No";
+        }
+    }
+}
