34c91dccb209f06fe9c47a4d0cf68583fe006844
Jeff Longo <jeffreylongo@yahoo.com>
2017-06-12 14:57:27 -0400

    Code Wars: TIY FIzzBuzz

---
+++ CWTIYFizzBuzz/CWTIYFizzBuzz/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Solution
+{
+    public static class TIY
+    {
+        public static string FizzBuzz(string sentence)
+        {
+            var vowels = "aeiouAEIOU";
+            var ans = "";
+
+            foreach (var item in sentence)
+            {
+                if (char.IsUpper(item) && vowels.Contains(item))
+                {
+                    ans += "Iron Yard";
+                }
+                if (char.IsUpper(item) && !vowels.Contains(item))
+                {
+                    ans += "Iron";
+                }
+                if ((char.IsLower(item) && !vowels.Contains(item)) || !char.IsLetter(item))
+                {
+                    ans += item;
+                }
+                if (char.IsLower(item) && vowels.Contains(item))
+                {
+                    ans += "Yard";
+                }
+            }
+            return ans;
+        }
+    }
+}
