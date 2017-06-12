7f4f3f65b50d7916c30cd34c3076576801571e19
Jeff Longo <jeffreylongo@yahoo.com>
2017-06-12 15:15:01 -0400

    Code Wars: Even or Odd

---
+++ CWEvenOrOdd/CWEvenOrOdd/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Solution
+{
+    public class SolutionClass
+    {
+        int number = Convert.ToInt32(Console.ReadLine());
+        public static string EvenOrOdd(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return "Even";
+            }
+            else
+            {
+                return "Odd";
+            }
+        }
+    }
+}
