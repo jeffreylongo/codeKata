646837d8fc5cdfd0452b8952955a097e0a0575b9
Jeff Longo <jeffreylongo@yahoo.com>
2017-06-12 15:29:56 -0400

    Hacker Rank: Pangrams

---
+++ HRPangrams/HRPangrams/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        string s = Console.ReadLine();
+        bool newS = s.ToLower().Where(c => Char.IsLetter(c)).GroupBy(c => c).Count() == 26;
+        if (newS)
+        {
+            Console.WriteLine(("pangram"));
+        }
+        else
+        {
+            Console.WriteLine("not pangram");
+        }
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+    }
+};
