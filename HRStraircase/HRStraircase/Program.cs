667ac40f07ff1df97f2c2780bce5aa1c62ea157c
Jeff Longo <jeffreylongo@yahoo.com>
2017-06-12 15:32:09 -0400

    Hacker Rank: Staircase

---
+++ HRStraircase/HRStraircase/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+        {
+            for (int e = 0; e < n - i; e++)
+            {
+                Console.Write(" ");
+            }
+            for (int v = 0; v < i; v++)
+            {
+                Console.Write("#");
+            }
+            Console.WriteLine("");
+        }
+        Console.ReadLine();
+    }
+}
