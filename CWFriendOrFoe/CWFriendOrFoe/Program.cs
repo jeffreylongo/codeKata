c44f223c950c3584d08a83e5f14730bbf615cece
Jeff Longo <jeffreylongo@yahoo.com>
2017-06-12 14:55:58 -0400

    Code Wars: Friend or Foe

---
+++ CWFriendOrFoe/CWFriendOrFoe/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Kata
+{
+    public static IEnumerable<string> FriendOrFoe(string[] names)
+    {
+        return names.Where(w => w.Length == 4).ToList();
+    }
+}
