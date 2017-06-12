5a79838d726b886e826b4c1abacbf062381ab821
Jeff Longo <jeffreylongo@yahoo.com>
2017-06-12 15:22:07 -0400

    Hacker Rank: Apple and Orange: C# and JavaScript

---
+++ HRAppleAndOrange/HRAppleAndOrange/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        string[] tokens_s = Console.ReadLine().Split(' ');
+        int s = Convert.ToInt32(tokens_s[0]);
+        int t = Convert.ToInt32(tokens_s[1]);
+        string[] tokens_a = Console.ReadLine().Split(' ');
+        int a = Convert.ToInt32(tokens_a[0]);
+        int b = Convert.ToInt32(tokens_a[1]);
+        string[] tokens_m = Console.ReadLine().Split(' ');
+        int m = Convert.ToInt32(tokens_m[0]);
+        int n = Convert.ToInt32(tokens_m[1]);
+        string[] apple_temp = Console.ReadLine().Split(' ');
+        int[] apple = Array.ConvertAll(apple_temp, Int32.Parse);
+        string[] orange_temp = Console.ReadLine().Split(' ');
+        int[] orange = Array.ConvertAll(orange_temp, Int32.Parse);
+
+        Console.WriteLine(apple.Where(x => x + a >= s && x + a <= t).Count().ToString());
+        Console.WriteLine(orange.Where(x => x + b >= s && x + b <= t).Count().ToString());
+    }
+}
