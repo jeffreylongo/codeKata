267099d919757310e8e8d8111bf2e7890f895fda
Jeff Longo <jeffreylongo@yahoo.com>
2017-06-12 14:58:21 -0400

    Code Wars: Exes and Ohs

---
+++ CWExsAndOhs/CWExsAndOhs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System;
+public static class Kata
+{
+    public static bool XO(string input)
+    {
+
+        var oCount = input.ToLower().Where(o => o == 'x').Count();
+        var xCount = input.ToLower().Where(x => x == 'o').Count();
+
+
+        if (oCount == xCount)
+        {
+            return true;
+        }
+        else if (oCount != xCount)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+
+    }
+}
