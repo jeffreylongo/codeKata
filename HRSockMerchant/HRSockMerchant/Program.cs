8d9c21b3f411c9bc1e3abdc1cb4183a3a9d3b57e
Jeff Longo <jeffreylongo@yahoo.com>
2017-06-12 15:28:58 -0400

    Hacker Rank: Sock Merchant

---
+++ HRSockMerchant/HRSockMerchant/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sock_Merchant
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] c_temp = Console.ReadLine().Split(' ');
+            int[] c = Array.ConvertAll(c_temp, Int32.Parse);
+
+            int noofpairs = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (c[i] == c[j] && c[j] != 0)
+                    {
+                        noofpairs++;
+                        c[j] = 0;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(noofpairs);
+        }
+    }
+}
