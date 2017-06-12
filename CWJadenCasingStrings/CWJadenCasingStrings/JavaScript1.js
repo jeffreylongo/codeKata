45c811dc919b014dc8e2f20192f85ed8f50f3087
Jeff Longo <jeffreylongo@yahoo.com>
2017-06-12 15:08:12 -0400

    Code Wars: Jaden Casing Strings: C# and JavaScript

---
+++ CWJadenCasingStrings/CWJadenCasingStrings/JavaScript1.js
@@ -0,0 +1,8 @@
+﻿String.prototype.toJadenCase = function () {
+    let splitted = this.split(" ");
+    for (let i = 0; i < splitted.length; i++) {
+        let thisString = splitted[i];
+        splitted[i] = splitted[i][0].toUpperCase() + splitted[i].slice(1);
+    }
+    return splitted.join(" ");
+};
---
+++ CWJadenCasingStrings/CWJadenCasingStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+public static class JadenCase
+{
+    public static string ToJadenCase(this string phrase)
+    {
+        var newString = String.Empty;
+        for (int i = 0; i < phrase.Length; i++)
+        {
+            var currentChar = phrase[i];
+            if ((i > 0 && phrase[i - 1] == ' ') || i == 0)
+            {
+                currentChar = char.Parse(phrase[i].ToString().ToUpper());
+            }
+            newString += currentChar;
+
+        }
+        return newString;
+    }
+}
