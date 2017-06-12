98e76053ba86801fcaef2955aa24ae25a74c0a2c
Jeff Longo <jeffreylongo@yahoo.com>
2017-06-12 15:10:17 -0400

    Code Wars: Keep up the Hoop

---
+++ CWKeepUpTheHood/CWKeepUpTheHood/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Kata
+{
+
+    public static string HoopCount(int n)
+    {
+        if (n >= 10)
+        {
+            return "Great, now move on to tricks";
+        }
+        else
+        {
+            return "Keep at it until you get it";
+        }
+    }
+}
---
+++ CWKeepUpTheHood/CWKeepUpTheHood/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CWKeepUpTheHood")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CWKeepUpTheHood")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fa6d8c3e-6867-4c95-ac92-ce97bf3b0cd2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
