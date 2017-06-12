d8f2bcfc8249ab49db27817910718d0727e2d96b
Jeff Longo <jeffreylongo@yahoo.com>
2017-06-12 15:15:41 -0400

    Code Wars: Fake Binary

---
+++ CWFakeBinary/CWFakeBinary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Kata
+{
+    public static string FakeBin(string x)
+    {
+        var rv = String.Empty;
+        //we are looping
+        for (int i = 0; i < x.Length; i++)
+        {
+            if (int.Parse(x[i].ToString()) >= 5)
+            {
+                //do something
+                rv += "1";
+            }
+            else
+            {
+                //do something else
+                rv += "0";
+            }
+        }
+        return rv;
+    }
+}
