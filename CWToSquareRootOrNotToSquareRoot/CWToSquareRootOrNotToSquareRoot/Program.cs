222e3399df101b581de0a5abd1fa080f5c7ad31f
Jeff Longo <jeffreylongo@yahoo.com>
2017-06-12 15:09:29 -0400

    Code Wars: To Square Root or Not to Square Root

---
+++ CWToSquareRootOrNotToSquareRoot/CWToSquareRootOrNotToSquareRoot/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Kata
+{
+    public static int[] SquareOrSquareRoot(int[] array)
+    {
+        var rv = new List<int>();
+        foreach (var number in array)
+        {
+            if (Math.Sqrt(number) == (int)Math.Sqrt(number))
+            {
+                rv.Add((int)Math.Sqrt(number));
+
+            }
+            else
+            {
+                rv.Add(number * number);
+
+            }
+        }
+        return rv.ToArray();
+    }
+}
