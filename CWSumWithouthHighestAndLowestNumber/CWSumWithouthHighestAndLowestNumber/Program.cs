f7f11e786c9a8ff91ebed8302ffccc6865d7419c
Jeff Longo <jeffreylongo@yahoo.com>
2017-06-12 15:11:52 -0400

    Code Wars: Sum Without Highest and Lowest Number

---
+++ CWSumWithouthHighestAndLowestNumber/CWSumWithouthHighestAndLowestNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static int Sum(int[] numbers)
+    {
+        if (numbers == null || numbers.Length <= 1)
+        {
+            return 0;
+        }
+        Array.Sort(numbers);
+        numbers[0] = 0;
+        numbers[numbers.Length - 1] = 0;
+        var sum = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sum += numbers[i];
+        }
+        return sum;
+    }
+}
