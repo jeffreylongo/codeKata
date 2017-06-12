4d1a9b0991bce9036bb4c99d4f8ca8d42291a673
Jeff Longo <jeffreylongo@yahoo.com>
2017-06-12 15:24:49 -0400

    Hacker Rank: Caesar Cipher

---
+++ HRCaesarCipher/HRCaesarCipher/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackerrank_Ceaser
+{
+    class Program
+    {
+        public static char GetNextCharacter(char c)
+        {
+            if (c == 'z')
+                return 'a';
+            else if (c == 'Z')
+                return 'A';
+            else
+                return (char)(c + (char)1);
+        }
+
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string str = Console.ReadLine();
+            int k = Convert.ToInt32(Console.ReadLine());
+            var output = String.Join("", str.Select(letter =>
+            {
+                var temp = letter;
+                // offset that by k
+                if (char.IsLetter(temp))
+                {
+                    for (int i = 0; i < k; i++)
+                    {
+                        temp = GetNextCharacter(temp);
+                    }
+                }
+                return temp;
+            }));
+            Console.WriteLine(output);
+
+        }
+    }
+}
