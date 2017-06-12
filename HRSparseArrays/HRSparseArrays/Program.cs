ee5faf836862fb9854961646b3c9a4e863b26857
Jeff Longo <jeffreylongo@yahoo.com>
2017-06-12 15:23:10 -0400

    Hacker Rank: Sparse Arrays

---
+++ HRSparseArrays/HRSparseArrays/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        // Inputs
+        var N = int.Parse(Console.ReadLine());
+        var dataset = new List<string>();
+        for (int i = 0; i < N; i++)
+        {
+            dataset.Add(Console.ReadLine());
+        }
+        var Q = int.Parse(Console.ReadLine());
+        var queries = new List<string>();
+        for (int i = 0; i < Q; i++)
+        {
+            queries.Add(Console.ReadLine());
+        }
+
+        var answers = new List<int>();
+
+        answers = queries.Select(query => dataset.Count(dataItem => query == dataItem)).ToList();
+
+        answers.ForEach(i => Console.WriteLine(i));
+    }
+}
