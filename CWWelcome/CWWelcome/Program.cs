eadb3ec028b48737f26ecd3396490af50cda0650
Jeff Longo <jeffreylongo@yahoo.com>
2017-06-13 09:21:57 -0400

    CodeWars: Welcome

---
+++ CWWelcome/CWWelcome/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+public static class Kata
+{
+    public static string Greet(string language)
+    {
+        var welcomes = new Dictionary<string, string>
+        {
+            {"english", "Welcome"},
+            {"czech", "Vitejte"},
+            {"danish", "Velkomst"},
+            {"dutch", "Welkom"},
+            {"estonian", "Tere tulemast"},
+            {"finnish", "Tervetuloa"},
+            {"flemish", "Welgekomen"},
+            {"french", "Bienvenue"},
+            {"german", "Willkommen"},
+            {"irish", "Failte"},
+            {"italian", "Benvenuto"},
+            {"latvian", "Gaidits"},
+            {"lithuanian", "Laukiamas"},
+            {"polish", "Witamy"},
+            {"spanish", "Bienvenido"},
+            {"swedish", "Valkommen"},
+            {"welsh", "Croeso"}
+        };
+
+        if (welcomes.ContainsKey(language))
+        {
+            return welcomes[language];
+        }
+        else
+        {
+            return "Welcome";
+        }
+
+    }
+}
