99b0ccfb7c81730a000ab79897c86062a925f5db
Jeff Longo <jeffreylongo@yahoo.com>
2017-06-22 10:15:46 -0400

    Code Wars: Filtering Even Numbers: Bug Fixes

---
+++ CWFilteringEvenNumbers/CWFilteringEvenNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+    public static List<int> FilterOddNumber(List<int> listOfNumbers)
+    {
+        for (int i = 0; i < listOfNumbers.Count; i++)
+        {
+            if (listOfNumbers[i] % 2 == 0)
+            {
+                listOfNumbers.RemoveAt(i--);
+            }
+        }
+        return listOfNumbers;
+    }
+}
