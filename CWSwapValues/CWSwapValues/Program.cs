7b3c42c07a6f797a51a8e5c6e465b90d932172de
Jeff Longo <jeffreylongo@yahoo.com>
2017-06-26 19:35:01 -0400

    Code Wars: Swapper.

---
+++ CWSwapValues/CWSwapValues/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Swapper
+{
+    public object[] Arguments { get; private set; }
+
+    public Swapper(object[] args)
+    {
+        Arguments = args;
+    }
+
+    public void SwapValues()
+    {
+        Arguments = new[] { Arguments[0], Arguments[1] };
+
+        object temp = Arguments[0];
+        Arguments[0] = Arguments[1];
+        Arguments[1] = temp;
+    }
+}
