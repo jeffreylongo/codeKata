d31364ac44db774eaba72826b57854ccbd9214bd
Jeff Longo <jeffreylongo@yahoo.com>
2017-06-26 09:25:22 -0400

    Code Wars: Finish Guessing Game C#

---
+++ CWFinishGame/CWFinishGame/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Guesser
+{
+    private int number;
+    private int lives;
+
+    public Guesser(int number, int lives)
+    {
+        this.number = number;
+        this.lives = lives;
+    }
+
+    public bool Guess(int n)
+    {
+        //throw error if no lives
+        if (lives < 1) throw new Exception("Error: No Lives");
+
+        //return false and take a life if guess is wrong
+        if (n != number)
+        {
+            lives--;
+            return false;
+        }
+
+        //return true if guess is correct
+        return true;
+    }
+
+
+}
