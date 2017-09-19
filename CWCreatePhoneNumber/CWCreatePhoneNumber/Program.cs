a0b87c12b082740fa44ae4c80caa7a4f38c2e2ea
Jeff Longo <jeffreylongo@yahoo.com>
2017-09-18 20:13:11 -0400

    Code Wars: C# Create Phone Number

---
+++ CWCreatePhoneNumber/CWCreatePhoneNumber/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Kata
+{
+    public static string CreatePhoneNumber(int[] numbers)
+    {
+        Int64 numbas = Convert.ToInt64(string.Join("", numbers));
+        return numbas.ToString("(000) 000-0000");
+    }
+}
