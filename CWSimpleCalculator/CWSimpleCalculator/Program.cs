446242649c801167d715c9dc2e77293cc38ba408
Jeff Longo <jeffreylongo@yahoo.com>
2017-09-18 20:23:12 -0400

    Code Wars: C# Simple Calculator

---
+++ CWSimpleCalculator/CWSimpleCalculator/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Kata
+{
+    public static double Calculator(double a, double b, char op)
+    {
+        if (op == '+')
+        {
+            return a + b;
+        }
+        else if (op == '-')
+        {
+            return a - b;
+        }
+        else if (op == '*')
+        {
+            return a * b;
+        }
+        else if (op == '/')
+        {
+            return a / b;
+        }
+        else
+        {
+            throw new System.ArgumentException("unknown value");
+        }
+    }
+}
