d9e522e804d6a6f2c62c0eaa401ef868f8916c06
Jeff Longo <jeffreylongo@yahoo.com>
2017-09-18 20:19:47 -0400

    Code Wars: Grasshopper - If/Else Debug

---
+++ CWGrasshopperIfElseDebug/CWGrasshopperIfElseDebug/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Player
+{
+    private int health = 100;
+    public int Health
+    {
+        get
+        {
+            return this.health;
+        }
+        set
+        {
+            health = value;
+        }
+    }
+
+    public Player()
+    {
+
+    }
+
+    //FIXME: This method won't compile for some reason! :(
+    public bool CheckAlive()
+    {
+        if
+        (this.Health <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
