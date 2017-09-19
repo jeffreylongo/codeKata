7953a1a5c9790e6da83ac4a3f7ff85da5c10cbb2
Jeff Longo <jeffreylongo@yahoo.com>
2017-09-18 20:18:05 -0400

    Code Wars: C# Do I Get A Bonus

---
+++ CWDoIGetABonus/CWDoIGetABonus/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public static class Kata
+{
+    public static string bonus_time(int salary, bool bonus)
+    {
+        var final = 0;
+        if (bonus)
+        {
+            final = salary * 10;
+        }
+        else
+        {
+            final = salary;
+        }
+        return ("$" + final);
+    }
+    public static string bonus_time2(int salary, bool bonus) => "$" + (bonus ? salary * 10 : salary);
+}
