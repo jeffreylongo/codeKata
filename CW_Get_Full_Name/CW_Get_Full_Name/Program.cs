4f7ee1edfb65642bcfaff8a47a34e6d4c955e0f4
Jeff Longo <jeffreylongo@yahoo.com>
2017-11-27 21:44:47 -0500

    Code Wars: C# Get Full Name Solution

---
+++ CW_Get_Full_Name/CW_Get_Full_Name/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Dinglemouse
+{
+    public static string firstName;
+    public static string lastName;
+    public string FullName
+    {
+        get
+        {
+            if (firstName == "")
+            {
+                firstName = String.Empty;
+                return lastName;
+            }
+            if (lastName == "")
+            {
+                lastName = String.Empty;
+                return firstName;
+            }
+            if (firstName == "" && lastName == "")
+            {
+                return String.Empty;
+            }
+            return firstName + " " + lastName;
+        }
+        set
+        {
+            
+        }
+    }
+
+    public Dinglemouse(string firstName, string lastName)
+    {
+        Dinglemouse.firstName = firstName;
+        Dinglemouse.lastName = lastName;
+    }
+}
