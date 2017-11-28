391d0003412210ebaeee7a6347b4fb59adb0a93f
Jeff Longo <jeffreylongo@yahoo.com>
2017-11-27 21:54:21 -0500

    Code Wars: C# Get Full Name Refactored Solution

---
+++ CW_Get_Full_Name_Refactor/CW_Get_Full_Name_Refactor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Dinglemouse
+{
+    private string firstName;
+    private string lastName;
+    public string FullName
+    {
+        get
+        {
+            return (firstName + " " + lastName).Trim();
+        }
+    }
+
+    public Dinglemouse(string firstName, string lastName)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
